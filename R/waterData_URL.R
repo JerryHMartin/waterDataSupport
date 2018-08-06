fd16391013a95af68da279b2b75783d570daae15
Jerry Martin <Agriculturist@users.noreply.github.com>
2018-08-06 11:37:22 -0400

    Add files via upload

---
+++ R/waterData_URL.R
@@ -0,0 +1,9 @@
+#' URLs associated with the waterData Support Package.
+#'
+#'
+#' @format a list of URLS
+#' \describe{
+#'   \item{name National_Hydrography_Dataset}{URL of national hydrography dataset}
+#'   ...
+#' }
+"waterData_URL"
