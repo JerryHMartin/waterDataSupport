ba2e127a96f63b4593822ef20ff84a33d76852a9
Jerry H. Martin <67657885+JerryHMartin@users.noreply.github.com>
2023-01-05 10:01:07 -0500

    removed unneeded text

--- builder.R
+++ builder.R
@@ -20,10 +20,7 @@ setwd(support_Dir)
 #devtools::create("waterDataSupport")
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> f561acd7fdbe6188b162e793aa3e88643d6dba29
 roxygen2::roxygenise()
 
 
@@ -42,11 +39,10 @@ devtools::check()
 
 testthat::test_file("tests/testthat/test-SiteFunctionsUSGS.R")
 
-<<<<<<< HEAD
-=======
+
 testthat::test_file("tests/testthat/test-Functions_NOAA_Stations.R")
 
 usethis::use_vignette("station_download")
 
->>>>>>> f561acd7fdbe6188b162e793aa3e88643d6dba29
+
 
